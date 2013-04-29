ff6803f79c0be2ed9c8f689832fc6d49a4d8e9ff
Joe Ketcham <joeketcham@gmail.com>
2013-04-29 18:00:36 -0400

    Added a getUser that returns an ArrayList of users

--- src/java/data/UserIO.java
+++ src/java/data/UserIO.java
@@ -5,6 +5,9 @@
 package data;
 import java.io.*;
 import business.User;
+import java.util.ArrayList;
+import java.util.NoSuchElementException;
+import java.util.StringTokenizer;
 /**
  *
  * @author jkk
@@ -19,6 +22,32 @@ public class UserIO {
                 + user.getFirstName() + "|" 
                 + user.getLastName());
         out.close();
+    }
+    public static ArrayList<User> getUsers(String filename) throws IOException
+    {
+        ArrayList<User> users = new ArrayList<User>();
+        BufferedReader in = new BufferedReader(
+                new FileReader(filename));
+        String line = in.readLine();
+        while (line != null)
+        {
+            try
+            {
+                StringTokenizer t = new StringTokenizer(line, "|");
+                String emailAddress = t.nextToken();
+                String firstName = t.nextToken();
+                String lastName = t.nextToken();
+                User user = new User(firstName, lastName, emailAddress);
+                users.add(user);
+                line = in.readLine();
+            }
+            catch(NoSuchElementException e)
+            {
+                line = in.readLine();                
+            }
+        }
+        in.close();
+        return users;
     }
     
 }
