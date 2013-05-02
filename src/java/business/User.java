a37b621357070655f880ef3f996ac1088e6eb829
Joe Ketcham <joeketcham@gmail.com>
2013-05-01 21:31:20 -0400

    undid commit of hashmap

--- src/java/business/User.java
+++ src/java/business/User.java
@@ -4,13 +4,7 @@
  */
 package business;
 
-import java.io.BufferedReader;
-import java.io.FileReader;
-import java.io.IOException;
 import java.io.Serializable;
-import java.util.HashMap;
-import java.util.NoSuchElementException;
-import java.util.StringTokenizer;
 /**
  *
  * @author jkk
@@ -52,30 +46,5 @@ public class User implements Serializable{
     public String getEmailAddress(){
         return emailAddress;
     }
-    public static HashMap<String, User> getUsersMap(String filename) throws IOException
-    {
-        HashMap<String, User> users = new HashMap<String, User>();
-        BufferedReader in = new BufferedReader(
-                new FileReader(filename));
-        String line = in.readLine();
-        while (line != null)
-        {
-            try
-            {
-                StringTokenizer t = new StringTokenizer(line, "|");
-                String emailAddress = t.nextToken();
-                String firstName = t.nextToken();
-                String lastName = t.nextToken();
-                User user = new User(firstName, lastName, emailAddress);
-                users.put(emailAddress, user);
-                line = in.readLine();
-            }
-            catch(NoSuchElementException e)
-            {
-                line = in.readLine();                
-            }
-        }
-        in.close();
-        return users;
-    }
+    
 }
