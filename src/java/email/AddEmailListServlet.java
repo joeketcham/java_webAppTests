9a71ff631aabe54206f765e7f64cc9310b228a2d
Joe Ketcham <joeketcham@gmail.com>
2013-05-04 19:59:30 -0400

    Testing JSTL to loop through a comma-delimited string

--- src/java/email/AddEmailListServlet.java
+++ src/java/email/AddEmailListServlet.java
@@ -8,6 +8,7 @@ import business.User;
 import data.UserIO;
 import java.util.ArrayList;
 import java.util.Date;
+import java.util.HashMap;
 import java.util.Set;
 
 public class AddEmailListServlet extends HttpServlet
@@ -33,9 +34,20 @@ public class AddEmailListServlet extends HttpServlet
         // store the User object in the request object
         request.setAttribute("user", user);
         
+        HashMap<String, User> usersMap = UserIO.getUsersMap(path);
+        request.setAttribute("usersMap", usersMap);
+        request.setAttribute("emailAddress", emailAddress);
+        
+        Cookie c = new Cookie("emailCookie", emailAddress);
+        c.setMaxAge(60*60); //set its age to 1 hour
+        c.setPath("/"); //allow the entire application to access it
+        response.addCookie(c);
+        
         // send the current date
         Date currentDate = new Date();
         request.setAttribute("currentDate", currentDate);
+        
+        request.setAttribute("codesTest", "8601,pf01,pf02,jr01");
         
         // send an array of colors to the view
         String[] colors = {"Red", "blue", "green"};
